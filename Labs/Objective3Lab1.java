bc618d34a8f71e78ec248a4e201af01d6b95672c
hopeful89 <brandon.stine89@gmail.com>
2021-04-24 11:25:05 -0400

    Objective3Lab1 complete duplicate of lab2/3

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+    //duplicate lab from objective2lab3
+  }
+}
