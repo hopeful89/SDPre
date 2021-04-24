1252a2d4b3a9057978033a1a6f94092f4eb545c4
hopeful89 <brandon.stine89@gmail.com>
2021-04-24 10:30:33 -0400

    completed objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,5 @@
+public class Objective1Lab2{
+  public static void main(String[] args){
+    System.out.println(" ----------------\n| Grocery List   |\n| 1. Milk        |\n| 2. Eggs        |\n| 3. Bread       |\n ----------------");
+  }
+}
