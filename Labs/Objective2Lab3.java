96591d0c8dab05a6d617df534ed054cb5026c74c
hopeful89 <brandon.stine89@gmail.com>
2021-04-24 11:12:57 -0400

    completed objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
