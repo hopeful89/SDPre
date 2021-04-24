efc75d5d00002ff7ba6091761881c1c17020db50
hopeful89 <brandon.stine89@gmail.com>
2021-04-24 11:04:41 -0400

    completed objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brandon Stine";
+    int age = 31;
+    String state = "North Carolina";
+    String introStatement = String.format("Name: %s Age: %s State: %s", name, age, state);
+
+    System.out.println(introStatement);
+  }
+}
