8ed65e6a37037d42d4385766ce9c0e83cd355a88
hopeful89 <brandon.stine89@gmail.com>
2021-04-24 11:45:39 -0400

    completed objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2{
+  public static void main(String[] args){
+    double side1;
+    double side2;
+    double hypotenuse;
+
+    side1 = Math.pow(10, 2);
+    side2 = Math.pow(8, 2);
+    hypotenuse = Math.sqrt(side1 + side2);
+
+    System.out.println(hypotenuse);
+  }
+}
