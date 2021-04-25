147b643d783e0849f3f3cb610f4de7abeec56409
hopeful89 <brandon.stine89@gmail.com>
2021-04-25 16:56:04 -0400

    completed Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,14 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    if(num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+    }
+    else if(num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1);
+
+    }
+  }
+}
