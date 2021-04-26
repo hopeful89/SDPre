e515f9b223d9d3d365589fc6a6a6159bba12963c
hopeful89 <brandon.stine89@gmail.com>
2021-04-26 15:33:01 -0400

    completed Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,10 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+      System.out.println(counter);
+      counter++;
+    }
+  }
+}
