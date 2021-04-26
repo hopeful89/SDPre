2d35f15d1e88a9e22e7bb9c762c3f22b9111db3c
hopeful89 <brandon.stine89@gmail.com>
2021-04-26 16:12:40 -0400

    completed Objective4Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int count = 0;
+    int sum = 0;
+
+    while(count < 20){
+      count++;
+      sum += count;
+    }
+    System.out.println(sum);
+  }
+}
