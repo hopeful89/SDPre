513a12f73d6a17c54bc5eeeef1060103542fea3f
hopeful89 <brandon.stine89@gmail.com>
2021-04-26 15:28:01 -0400

    completed Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,10 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+      System.out.println(counter);
+      counter = counter - 1; //this makes we count down
+    }
+  }
+}
