a2072c0292a97c57dffdcf8cf890685a87f05e44
hopeful89 <brandon.stine89@gmail.com>
2021-04-27 16:25:45 -0400

    completed  Objective9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,12 @@
+public class Objective9Lab1 {
+  public static void main(String[] args){
+    /*
+    Your solution goes here
+    */
+    printName();
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
