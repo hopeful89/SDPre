2977faa5f1ea60e8ddf2cf998d131d35c8ab2bf8
hopeful89 <brandon.stine89@gmail.com>
2021-04-27 16:35:03 -0400

    completed Objective9Lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,11 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    System.out.println("In method");
+  }
+}
