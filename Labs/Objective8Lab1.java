378de0deb7d67be16756ce4e7ade5e7dfd655b41
hopeful89 <brandon.stine89@gmail.com>
2021-04-27 16:01:10 -0400

    completed Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1-10
+    for(int i = 1; i < 11; i++){
+      System.out.println(i);
+    };
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+    for(int i = 10; i >= 1; i--){
+      System.out.println(i);
+    };
+
+  }
+}
