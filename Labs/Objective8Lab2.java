f86c1dc82ef181a7a129dbedf1c950597b1e6cbe
hopeful89 <brandon.stine89@gmail.com>
2021-04-27 16:08:14 -0400

    completed Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    /*TODO Write a for loop. sum of 1 -> 20
+    Increment the value of your control with each iteration.
+    Update the current sum by adding your control variable to sum with each iteration. */
+    int sum = 0;
+
+    for(int i = 0; i <= 20; i++){
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
